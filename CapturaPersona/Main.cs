bde6bc17aba092197c8b647e5799ba8c89943dd7
ShidoDarkland <shidodarkland@shidodarkland.(none)>
2013-09-26 17:35:52 -0600

    Agregado de los parametros a la clase persona y del menu principal

--- CapturaPersona/Main.cs
+++ CapturaPersona/Main.cs
@@ -6,7 +6,34 @@ namespace CapturaPersona
 	{
 		public static void Main (string[] args)
 		{
-			Console.WriteLine ("Hello World!");
+			int sel;
+
+			Console.WriteLine ("Bienvenido al directorio de Personas \n Menu:\n1)Capturar nueva Persona\n2)Modificar Datos de una Persona\n3)Borrar Persona ");
+			sel = Convert.ToInt32(Console.ReadLine());
+			Console.Clear();
+
+			switch (sel) {
+
+			case 1:
+				Console.WriteLine ("+++Bienvenido+++");
+				Console.ReadKey(true);
+				break;
+				
+			case 2:
+				Console.WriteLine("+++Bienvenido+++");
+				Console.ReadKey(true);
+				break;
+
+			case 3:
+				Console.WriteLine("+++Bienvenido+++");
+				Console.ReadKey(true);
+				break;
+
+
+			Default: 
+					Console.WriteLine("Seleccion Erronea");
+
+			}
 		}
 	}
 }
--- CapturaPersona/Persona.cs
+++ CapturaPersona/Persona.cs
@@ -4,9 +4,16 @@ namespace CapturaPersona
 {
 	public class Persona
 	{
+		public int codigo;
+		public string nombre;
+		public string telefono;
+		public string faceTag;
+
 		public Persona ()
 		{
 		}
+
+
 	}
 }
 
